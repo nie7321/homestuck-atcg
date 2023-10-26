5d6ffb7b17355ad8cad5a3b7055123346d3230dd
Nick Evans <nick.evans@northwestern.edu>
2023-10-25 19:45:40 -0500

    Eject undesirable header quicklinks
    They don't look good, and the ToC will do the job just as well

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import { toString } from 'hast-util-to-string'
-import { h } from 'hastscript'
-
-const createSROnlyLabel = (text) => {
-  const node = h('span.sr-only', `Section: ${escape(text)}`)
-  node.properties['is:raw'] = true
-  return node
-}
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,26 +9,6 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [
       rehypeHeadingIds,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'append',
-          group: ({ tagName }) =>
-              h(`div.heading-wrapper.level-${tagName}`, {
-                tabIndex: -1,
-              }),
-          content: (heading) => [
-            h(
-                `span.anchor-icon`,
-                {
-                  ariaHidden: 'true',
-                },
-                '#'
-            ),
-            createSROnlyLabel(toString(heading)),
-          ],
-        }
-      ]
     ],
   }
 });
