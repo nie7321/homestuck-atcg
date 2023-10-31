aca6ee13c8e38482023646e2ea7147b51a8041a0
Nick Evans <nick.evans@northwestern.edu>
2023-10-31 18:54:10 -0500

    Fix UFoC from dropdowns

--- src/scripts/initialize-dt.js
+++ src/scripts/initialize-dt.js
@@ -1,10 +1,12 @@
 import { DataTable, addColumnFilter } from "simple-datatables";
 
-const dt = new DataTable('#cards', {
-    paging: false,
+document.addEventListener('DOMContentLoaded', function(event) {
+    const dt = new DataTable('#cards', {
+        paging: false,
+    });
+
+    addColumnFilter(dt);
+
+    document.querySelector('.datatable-search .datatable-input').setAttribute('aria-label', 'Search');
+    document.querySelector('nav.datatable-pagination').setAttribute('aria-label', 'Card Page');
 });
-
-addColumnFilter(dt);
-
-document.querySelector('.datatable-search .datatable-input').setAttribute('aria-label', 'Search');
-document.querySelector('nav.datatable-pagination').setAttribute('aria-label', 'Card Page');
